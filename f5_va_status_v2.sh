ca3eb69c82c377de950c37e962a97d350af89da5
rajibr <82666308+rajibrajkhowa@users.noreply.github.com>
2025-03-17 13:32:10 +0530

    Create f5_va_status_v2.sh
    Initial commit

---
+++ f5_va_status_v2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Variables Passed As Arguments
+F5_HOST=$1
+F5_USER=$2
+F5_PASS=$(<password.txt)
+VIRTUAL_ADDRESS=$3
+
+# Function to get the status of a virtual address
+get_virtual_address_status() {
+    curl -sk -u $F5_USER:$F5_PASS \
+    -H "Content-Type: application/json" \
+    -X GET "https://$F5_HOST/mgmt/tm/ltm/virtual-address/$VIRTUAL_ADDRESS"
+}
+
+echo
+
+
+# Call the function
+VIP_STATUS=$(get_virtual_address_status | jq '.enabled' | tr -d '"')
+
+jq -nc \
+--arg vip "$VIRTUAL_ADDRESS" \
+--arg vip_status "$VIP_STATUS" \
+'{
+   "VIP": $vip,
+   "VIP_ENABLED": $vip_status
+  }'
